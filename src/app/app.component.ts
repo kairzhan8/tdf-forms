1224bcdab8dae000c727c4ace0822eb85a05ed04
kairzhan <kairzhankural@gmail.com>
2019-03-03 19:41:37 +0600

    Binding Data to a Model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   topics = ['Angular', 'React', 'Vue'];
+
+  userModel = new User('Sergio', 'Ramos', 'sr4@gmail.com', 87056541256, '', 'evening', true);
 }
